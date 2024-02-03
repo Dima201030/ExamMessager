05ea9dae02400b73d6659934d5bd58cbb77954ad
dima <dima@kojem.ru>
2024-02-03 15:32:07 +0300

    General 1

---
+++ ExamMessager/Core/ActiveNow/ActiveNowView.swift
@@ -0,0 +1,18 @@
+//
+//  ActiveNowView.swift
+//  ExamMessager
+//
+//  Created by Дима Кожемякин on 03.02.2024.
+//
+
+import SwiftUI
+
+struct ActiveNowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActiveNowView()
+}
