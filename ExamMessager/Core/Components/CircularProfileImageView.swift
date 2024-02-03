b395ec23dc7970dc26bf7ed5e7a5c36cc62b983a
dima <dima@kojem.ru>
2024-02-03 22:46:59 +0300

    General 3

---
+++ ExamMessager/Core/Components/CircularProfileImageView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProfileImageView.swift
+//  ExamMessager
+//
+//  Created by Дима Кожемякин on 03.02.2024.
+//
+
+import SwiftUI
+
+struct CircularProfileImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProfileImageView()
+}
---
+++ ExamMessager/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  ExamMessager
+//
+//  Created by Дима Кожемякин on 03.02.2024.
+//
+
+import Foundation
